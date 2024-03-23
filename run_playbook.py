5506f65ed563ed306e538e352b455d4a6947d4ea
TahaK7534 <113482366+TahaK7534@users.noreply.github.com>
2024-03-23 21:47:16 +0000

    final changes

--- run_playbook.py
+++ run_playbook.py
@@ -1,23 +1,21 @@
 import ansible_runner
-import json
 import os
 
+# Set the ANSIBLE_CONFIG environment variable to point to the ansible.cfg file
 os.environ['ANSIBLE_CONFIG'] = os.path.join(os.getcwd(), 'ansible.cfg')
-# Run the hello.yml playbook on the 'app' group
-results = ansible_runner.run(private_data_dir='./', playbook='hello.yml', extravars={'target_group': 'app'})
 
-# Process the results
-for each_host in results.events:
-    print(each_host['event'])
+# Run the hello.yml playbook using ansible-playbook command
+result = ansible_runner.run_command(executable_cmd='ansible-playbook', cmdline_args=['hello.yml'])
 
-# Verify the response of the NodeJS servers
-nodejs_servers_response = {}
-for each_event in results.events:
-    if 'event_data' in each_event and 'stdout' in each_event['event_data']:
-        server_name = each_event['event_data']['stdout'].split()[1]
-        response = each_event['event_data']['stdout_lines'][1]
-        nodejs_servers_response[server_name] = response
+start_pos = result[0].find("TASK [Check list of Node.js apps running.]")
+end_pos = result[0].find("TASK [Start example Node.js app.]")
+output_section = result[0][start_pos:end_pos]
+# Count the occurrences of "ok" in the output section
+ok_count = output_section.count("ok")
 
-# # Print the response of NodeJS servers
-# print("\nResponse of NodeJS servers:")
-# print(json.dumps(nodejs_servers_response, indent=4))
+# Check if the count is equal to 3
+if ok_count == 3:
+    print("All three the NodeJS servers  are OK for the specified task.")
+else:
+    print("Not all three the NodeJS servers are OK for the specified task.")
+
